faf98f2da0d990dcaaf0dcb388093d319ec82a67
ItayTk <130302469+ItayTk@users.noreply.github.com>
2024-02-16 16:22:57 +0200

    add this

---
+++ Set_Card_Game/src/main/java/bguspl/set/Dealer.java
@@ -0,0 +1,199 @@
+package bguspl.set.ex;
+
+import bguspl.set.Env;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * This class manages the dealer's threads and data
+ */
+public class Dealer implements Runnable {
+
+    /**
+     * The game environment object.
+     */
+    private final Env env;
+
+    /**
+     * Game entities.
+     */
+    private final Table table;
+    private final Player[] players;
+
+    /**
+     * The list of card ids that are left in the dealer's deck.
+     */
+    private final List<Integer> deck;
+
+    /**
+     * True iff game should be terminated.
+     */
+    private volatile boolean terminate;
+
+    private Queue<Integer> waitingPlayers;
+
+    /**
+     * The time when the dealer needs to reshuffle the deck due to turn timeout.
+     */
+    private long reshuffleTime = Long.MAX_VALUE;
+
+    private boolean shouldReset;
+
+    public Dealer(Env env, Table table, Player[] players) {
+        this.env = env;
+        this.table = table;
+        this.players = players;
+        deck = IntStream.range(0, env.config.deckSize).boxed().collect(Collectors.toList());
+        waitingPlayers = new LinkedList<Integer>();
+        shouldReset = false;
+    }
+
+    /**
+     * The dealer thread starts here (main loop for the dealer thread).
+     */
+    @Override
+    public void run() {
+        for (int i = 0; i < players.length; i++) {
+            Thread player = new Thread(players[i]);
+            player.start();
+        }
+        reshuffleTime = System.currentTimeMillis() + env.config.turnTimeoutMillis;
+        env.logger.info("thread " + Thread.currentThread().getName() + " starting.");
+        while (!shouldFinish()) {
+            placeCardsOnTable();
+            timerLoop();
+            updateTimerDisplay(shouldReset);
+            removeAllCardsFromTable();
+        }
+        announceWinners();
+        env.logger.info("thread " + Thread.currentThread().getName() + " terminated.");
+    }
+
+    /**
+     * The inner loop of the dealer thread that runs as long as the countdown did
+     * not time out.
+     */
+    private void timerLoop() {
+        while (!terminate && System.currentTimeMillis() < reshuffleTime) {
+            sleepUntilWokenOrTimeout();
+            updateTimerDisplay(shouldReset);
+            removeCardsFromTable();
+            placeCardsOnTable();
+        }
+    }
+
+    /**
+     * Called when the game should be terminated.
+     */
+    public void terminate() {
+        // TODO implement
+        terminate = true;
+    }
+
+    /**
+     * Check if the game should be terminated or the game end conditions are met.
+     *
+     * @return true iff the game should be finished.
+     */
+    private boolean shouldFinish() {
+        return terminate || env.util.findSets(deck, 1).size() == 0;
+    }
+
+    /**
+     * Checks cards should be removed from the table and removes them.
+     */
+    private void removeCardsFromTable() {
+        // TODO implement
+        synchronized (table) {
+            if (reshuffleTime - System.currentTimeMillis() <= 0) {
+                for (int i = 0; i < table.tokenToSlot.length; i++) {
+                    for (int j = 0; j < table.tokenToSlot[i].length; j++) {
+                        table.tokenToSlot[i][j] = false;
+                        table.removeToken(i, j);
+                    }
+                }
+                for (int i = 0; i < players.length; i++) {
+                    synchronized (players[i]) {
+                        players[i].tableCleared();
+                    }
+                }
+                for (int i = 0; i < table.slotToCard.length; i++) {
+                    deck.add(table.slotToCard[i]);
+                    table.slotToCard[i] = null;
+                    table.removeCard(i);
+                }
+
+                reshuffleTime = System.currentTimeMillis() + env.config.turnTimeoutMillis;
+                shouldReset = true;
+            } else {
+
+            }
+        }
+    }
+
+    /**
+     * Check if any cards can be removed from the deck and placed on the table.
+     */
+    private void placeCardsOnTable() {
+        // TODO implement
+        Collections.shuffle(deck);
+        while (!deck.isEmpty() && table.countCards() < 12) {
+            List<Integer> empties = table.getAllEmptySlots();
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(empties.size());
+            table.placeCard(deck.get(0), empties.get(randomIndex));
+            deck.remove(0);
+            empties.remove(randomIndex);
+        }
+    }
+
+    /**
+     * Sleep for a fixed amount of time or until the thread is awakened for some
+     * purpose.
+     */
+    private void sleepUntilWokenOrTimeout() {
+        // TODO implement
+    }
+
+    /**
+     * Reset and/or update the countdown and the countdown display.
+     */
+    private void updateTimerDisplay(boolean reset) {
+        // TODO implement
+        boolean color = false;
+        if (reset) {
+            env.ui.setCountdown(env.config.turnTimeoutMillis, color);
+            shouldReset = false;
+        } else {
+            if (reshuffleTime - System.currentTimeMillis() < env.config.turnTimeoutWarningMillis)
+                color = true;
+            env.ui.setCountdown(reshuffleTime - System.currentTimeMillis(), color);
+        }
+
+    }
+
+    /**
+     * Returns all the cards from the table to the deck.
+     */
+    private void removeAllCardsFromTable() {
+        // TODO implement
+    }
+
+    /**
+     * Check who is/are the winner/s and displays them.
+     */
+    private void announceWinners() {
+        // TODO implement
+    }
+
+    public void addToWaiting(int id) {
+        waitingPlayers.add(id);
+    }
+
+}
